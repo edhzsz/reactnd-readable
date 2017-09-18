24abd586e8836467b6da07f60c5c012356d37895
Edgar Hernandez <edhzsz@gmail.com>
2017-09-18 23:25:41 +0200

    code: Add the side bar and initial posts layout.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,39 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="navbar">
+        <div className="header">
           <h1>Readable</h1>
         </div>
         <div className="main">
+          <div className="side-bar">
+            <h2 className="title">Categories</h2>
+            <ul>
+              <li>React</li>
+              <li>Redux</li>
+              <li>Udacity</li>
+              <li>+ Add Category</li>
+            </ul>
+          </div>
           <div className="content">
-            Hello World!
-            <button type="button" className="btn-rect primary">
-              <span>Primary</span>
-            </button>
-            <button type="button" className="btn-rect secondary">
-              <span>Secondary</span>
-            </button>
+            <div className="post-list">
+              <div className="post">
+                <div className="post-header">
+                  <span>Udacity is the best place to learn React</span>
+                </div>
+                <div className="post-contents">
+                  <div className="post-main">
+                    Everyone says so after all.
+                  </div>
+                  <div className="comments-list">
+                    Comments
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="footer">
-          
+
         </div>
       </div>
     );
