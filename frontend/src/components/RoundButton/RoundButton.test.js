3835bfda29284f7634beb0494df35aff91b2c4c5
Edgar Hernandez <edhzsz@gmail.com>
2017-09-29 22:39:11 +0200

    code: Add props for the button size and background image

--- frontend/src/components/RoundButton/RoundButton.test.js
+++ frontend/src/components/RoundButton/RoundButton.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import RoundButton from '.';
 
@@ -17,8 +17,14 @@ describe('Component: RoundButton', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('should match its empty snapshot', () => {
-    const button = shallow(<RoundButton className="add-post primary">Add a Book</RoundButton>)
+  it('should use the className and children', () => {
+    const button = shallow(<RoundButton className="add-post">Add a Book</RoundButton>)
+    const tree = toJson(button);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should use the size and background properties for the inner span style', () => {
+    const button = mount(<RoundButton backgroundImage="./test.jpg" size={30} />)
     const tree = toJson(button);
     expect(tree).toMatchSnapshot();
   });
--- frontend/src/components/RoundButton/index.js
+++ frontend/src/components/RoundButton/index.js
@@ -3,9 +3,19 @@ import PropTypes from 'prop-types';
 import './RoundButton.css'
 
 const RoundButton = (props) => {
+  const style = {
+    width: props.size,
+    height: props.size,
+    backgroundSize: props.size / 2
+  }
+
+  if(props.backgroundImage) {
+    style.backgroundImage = `url(${props.backgroundImage})`;
+  }
+
   return (
     <div className={props.className}>
-      <a className={`btn-circle ${ props.type }`} href="#addpost">{props.children}</a>
+      <span className={`btn-circle ${ props.type }`} style={style} >{props.children}</span>
     </div>
   );
 }
@@ -13,11 +23,14 @@ const RoundButton = (props) => {
 RoundButton.propTypes = {
   className: PropTypes.string,
   type: PropTypes.oneOf(['primary', 'secondary']).isRequired,
+  backgroundImage: PropTypes.string,
+  size: PropTypes.number,
 }
 
 // Specifies the default values for props:
 RoundButton.defaultProps = {
-  type: 'primary'
+  type: 'primary',
+  size: 50
 };
 
 export default RoundButton;
