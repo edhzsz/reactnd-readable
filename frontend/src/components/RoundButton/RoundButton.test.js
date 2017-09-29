b1120a975355c6b90256b5e58e41160a229e6c33
Edgar Hernandez <edhzsz@gmail.com>
2017-09-29 21:54:00 +0200

    code: Add the RoundButton component

--- frontend/src/components/RoundButton/RoundButton.test.js
+++ frontend/src/components/RoundButton/RoundButton.test.js
@@ -6,16 +6,20 @@ import RoundButton from '.';
 
 const wrapper = shallow(<RoundButton/>);
 
-describe('(RoundButton) RoundButton', () => {
+describe('Component: RoundButton', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<RoundButton />, div);
   });
 
-
   it('should match its empty snapshot', () => {
     const tree = toJson(wrapper);
+    expect(tree).toMatchSnapshot();
+  });
 
+  it('should match its empty snapshot', () => {
+    const button = shallow(<RoundButton className="add-post primary">Add a Book</RoundButton>)
+    const tree = toJson(button);
     expect(tree).toMatchSnapshot();
   });
 
--- frontend/src/components/RoundButton/index.js
+++ frontend/src/components/RoundButton/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './RoundButton.css'
 
-const RoundButton = () => {
+const RoundButton = (props) => {
   return (
-    <div>
+    <div className={props.className}>
+      <a className={`btn-circle ${ props.type }`} href="#addpost">{props.children}</a>
     </div>
   );
 }
 
+RoundButton.propTypes = {
+  className: PropTypes.string,
+  type: PropTypes.oneOf(['primary', 'secondary']).isRequired,
+}
+
+// Specifies the default values for props:
+RoundButton.defaultProps = {
+  type: 'primary'
+};
 
 export default RoundButton;
