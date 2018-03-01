6da541250d4eea5ee0bea3f30c8c634b2b6cdd25
Edgar Hernandez <edhzsz@gmail.com>
2018-03-01 21:20:06 +0000

    code: create the redux store using the root reducer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
