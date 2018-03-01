496c630bff4a2593fa9674746deeb26ff8ed239e
Edgar Hernandez <edhzsz@gmail.com>
2018-03-01 21:22:17 +0000

    code: add support for the __REDUX_DEVTOOLS_EXTENSION__

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
