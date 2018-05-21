d99c24bb09df0f9b238e017d5f8a9104a801e6ec
Edgar Hernandez <edhzsz@gmail.com>
2018-05-21 22:59:10 +0100

    code: Add action types constants.
    Probably a lot more are missing but need to start somewhere.

--- frontend/src/constants/ActionTypes.js
+++ frontend/src/constants/ActionTypes.js
@@ -1,1 +1,13 @@
 export const VIEW_CATEGORY = 'VIEW_CATEGORY';
+export const SORT_POSTS_BY = 'SORT_BY';
+export const VIEW_POST_DETAIL = 'VIEW_POST_DETAIL';
+export const ADD_POST = 'ADD_POST';
+export const EDIT_POST = 'EDIT_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const SAVE_POST = 'SAVE_POST';
+export const UPVOTE_POST = 'UPVOTE_POST';
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const SAVE_COMMENT = 'SAVE_COMMENT';
+export const UPVOTE_COMMENT = 'UPVOTE_COMMENT';
